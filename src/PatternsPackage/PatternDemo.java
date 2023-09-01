4a231f0f1d0030cbea3b2f1a00dcdacb98babb7e
Selva Ganesh M <selvadev2k@gmail.com>
2023-09-01 17:54:51 +0530

    pattern5

--- src/PatternsPackage/PatternDemo.java
+++ src/PatternsPackage/PatternDemo.java
@@ -11,13 +11,25 @@ public class PatternDemo {
 		}
 	}
 	
+	private static void p5(int num) {
+		for (int i=0; i<num; i++) {
+			for (int j=num-i; j>0; j--) {
+				System.out.print("* ");
+			}
+			System.out.println("\n");
+		}
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		Pattern1 pattern1 = new Pattern1(5);
 //		pattern1.drawPattern();
 //		new Pattern2().drawPattern(7);
 //		new Pattern3().drawPattern(8);
-		p4(5);
+//		p4(5);
+		p5(5);
 	}
 
 }
