ccf1068031b6aa3b2a8d7ac86991d3181a8cf890
Selva Ganesh M <selvadev2k@gmail.com>
2023-09-01 17:50:48 +0530

    pattern4

--- src/PatternsPackage/PatternDemo.java
+++ src/PatternsPackage/PatternDemo.java
@@ -2,12 +2,22 @@ package PatternsPackage;
 
 public class PatternDemo {
 
+	private static void p4(int num) {
+		for (int i=0; i<num; i++) {
+			for (int j=0; j<i+1; j++) {
+				System.out.print((i+1)+" ");
+			}
+			System.out.println("\n");
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		Pattern1 pattern1 = new Pattern1(5);
 //		pattern1.drawPattern();
 //		new Pattern2().drawPattern(7);
-		new Pattern3().drawPattern(8);
+//		new Pattern3().drawPattern(8);
+		p4(5);
 	}
 
 }
